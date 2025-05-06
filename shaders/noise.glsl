51cfec1221c444630ec3be8e9cf60ded5247ba68
zxfish <27432931+zxfish@users.noreply.github.com>
2025-05-06 21:41:23 +0200

    Update noise.glsl
    add 1d noise

---
+++ shaders/noise.glsl
@@ -0,0 +1,42 @@
+// 1D noise
+
+// By Morgan McGuire @morgan3d, http://graphicscodex.com
+// Reuse permitted under the BSD license.
+
+// All noise functions are designed for values on integer scale.
+// They are tuned to avoid visible periodicity for both positive and
+// negative coordinates within a few orders of magnitude.
+
+#define NUM_NOISE_OCTAVES 5
+
+// Precision-adjusted variations of https://www.shadertoy.com/view/4djSRW
+float hash(float p) { p = fract(p * 0.011); p *= p + 7.5; p *= p + p; return fract(p); }
+
+float noise(float x) {
+    float i = floor(x);
+    float f = fract(x);
+    float u = f * f * (3.0 - 2.0 * f);
+    return mix(hash(i), hash(i + 1.0), u);
+}
+
+
+float fbm(float x) {
+	float v = 0.0;
+	float a = 0.5;
+	float shift = float(100);
+	for (int i = 0; i < NUM_NOISE_OCTAVES; ++i) {
+		v += a * noise(x);
+		x = x * 2.0 + shift;
+		a *= 0.5;
+	}
+	return v;
+}
+
+
+void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
+	float v = 0.0;
+	float coord = fragCoord.x * 0.05 + iTime * 5.0 - 10.0;
+    float height = NOISE(coord) * iResolution.y / 2.0;
+    v = clamp((height - fragCoord.y + iResolution.y / 2.0) / (iResolution.y * 0.02), 0.0, 1.0);	
+	fragColor.rgb = pow(v, 0.35) * 1.3 * normalize(vec3(0.5, fragCoord.xy / iResolution.xy)) + vec3(v * 0.25);
+}
